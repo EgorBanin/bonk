7e62770cd1c40e3fc843ddc1cdb7b3ee8ca4139f
Georgy Banin <gbanin@avito.ru>
2018-02-01 21:47:31 +0300

    класс для сборки multipart/form-data

--- src/http.php
+++ src/http.php
@@ -45,3 +45,90 @@ function http_request($method, $url, array $headers, $body, array $options = [])
 	];
 }
 
+class MultipartFormData {
+
+	private $params = [];
+
+	public function addParam(array $headers, $value) {
+		$this->params[] = [
+			'headers' => $headers,
+			'value' => $value,
+		];
+
+		return true;
+	}
+
+	public function addTextParam($name, $value, $charset = null) {
+		$headers = ['Content-Disposition: form-data; name="' . $name . '"'];
+
+		if ($charset) {
+			$headers[] = 'Content-Type: text/plain; charset=' . $charset;
+		}
+
+		return $this->addParam($headers, $value);
+	}
+
+	public function addFileParam($name, $value, $filename = null, $type = null) {
+		$disposition =  'Content-Disposition: form-data; name="' . $name . '"';
+		if ($filename) {
+			$disposition .= '; filename="' . $filename . '"';
+		}
+		$headers = [$disposition];
+
+		if ($type) {
+			$headers[] = 'Content-Type: ' . $type;
+		}
+
+		return $this->addParam($headers, $value);
+	}
+
+	public function loadFile($name, $filename, $type = null) {
+		if ( ! is_readable($filename)) {
+			throw new \Exception('Не удалось прочитать файл ' . $filename);
+		}
+		$value = file_get_contents($filename);
+
+		if ( ! $type) {
+			$finfo = new finfo(FILEINFO_MIME);
+			$type = $finfo->file($filename);
+		}
+
+		return $this->addFileParam($name, $value, basename($filename), $type);
+	}
+
+	public function encode() {
+		$boundary = $this->generateBoundary(10);
+		if ($boundary === null) {
+			throw new \Exception('Не удалось сгенерировать boundary');
+		}
+
+		$eol = "\r\n";
+		$body = '';
+		foreach ($this->params as $param) {
+			$body .= '--' . $boundary . $eol;
+			$body .= implode($eol, $param['headers']) . $eol;
+			$body .= $eol;
+			$body .= $param['value'] . $eol;
+		}
+		$body .= '--' . $boundary . '--' . $eol;
+		$body .= $eol;
+
+		return [$boundary, $body];
+	}
+
+	private function generateBoundary($tryLimit) {
+		do {
+			--$tryLimit;
+			$boundary = bin2hex(random_bytes(10));
+			foreach ($this->params as &$param) {
+				if (strpos($param['value'], $boundary) !== false) {
+					$boundary = null;
+					break;
+				}
+			}
+		} while ($boundary === null && $tryLimit > 0);
+
+		return $boundary;
+	}
+
+}
